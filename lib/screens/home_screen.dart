e160bc587866f3951d56134b8791a2d4b36e42ef
Klaudius-netizen <klaudiusaudieirsansaputra_2327250018@mhs.mdp.ac.id>
2025-03-05 09:53:28 +0700

    hmm

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:pilem/models/movie.dart';
 import 'package:pilem/screens/detail_screen.dart';
 import 'package:pilem/services/api_service.dart';
+import 'package:animations/animations.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,24 +12,23 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
   final ApiServices _apiServices = ApiServices();
   List<Movie> _allMovies = [];
   List<Movie> _trendingMovies = [];
   List<Movie> _popularMovies = [];
+  late AnimationController _animationController;
 
   Future<void> _loadMovies() async {
     final movies = await _apiServices.getAllMovies();
     final trending = await _apiServices.getTrendingMovies();
     final popular = await _apiServices.getPopularMovies();
 
-
-
-
     setState(() {
       _allMovies = movies;
-    _trendingMovies = trending;
-    _popularMovies = popular;
+      _trendingMovies = trending;
+      _popularMovies = popular;
     });
   }
 
@@ -35,60 +36,140 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
     _loadMovies();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    )..forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Pilem')),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            _buildMoviesList("All Movies", _allMovies),   
-            _buildMoviesList("Trending Movies", _trendingMovies),
-            _buildMoviesList("Popular Movies", _popularMovies),
-          ],
+    return Theme(
+      data: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black87,
+        appBarTheme: AppBarTheme(backgroundColor: Colors.black45, elevation: 0),
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: Colors.white70),
+          bodyMedium: TextStyle(color: Colors.white70),
         ),
-      )
-        
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Pilem', style: TextStyle(color: Colors.white)),
+        ),
+        body: FadeTransition(
+          opacity: _animationController,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildMoviesList("All Movies", _allMovies),
+                _buildMoviesList("Trending Movies", _trendingMovies),
+                _buildMoviesList("Popular Movies", _popularMovies),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
-  Widget _buildMoviesList(String title, List<Movie> movies){
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Padding(padding: EdgeInsets.all(8),
-      child: Text(title,style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-      ),
-      ),SizedBox(
-        height: 200,child: ListView.builder(scrollDirection: Axis.horizontal,
-        itemCount: movies.length,
-        itemBuilder: (BuildContext context, int index) {
-          final Movie movie = movies[index];
-          return GestureDetector(
-            onTap: () => Navigator.push(context, 
-            MaterialPageRoute(builder: (context) => DetailScreen(movie: movie,))
-            ),
-            child: Padding(padding: EdgeInsets.all(8.0),child: Column(
-            children: [
-              Image.network('https://image.tmdb.org/t/p/w500${movie.posterPath}',height: 150,
-              width: 120,
-              fit: BoxFit.cover,),
-              SizedBox(height: 5,),
-              Text(movie.title.length > 14
-              ? '${movie.title.substring(0, 10)}...'
-              : movie.title,
-              style: const TextStyle(
 
-              ),)
-            ],
+  Widget _buildMoviesList(String title, List<Movie> movies) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
           ),
-          ),
-          );
-        },
         ),
-      )
-    ],
-  );
-}
+        SizedBox(
+          height: 250,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: movies.length,
+            itemBuilder: (BuildContext context, int index) {
+              final Movie movie = movies[index];
+              return OpenContainer(
+                closedElevation: 0,
+                openElevation: 0,
+                closedColor: Colors.transparent,
+                openColor: Colors.transparent,
+                closedBuilder:
+                    (context, action) => GestureDetector(
+                      onTap: action,
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Hero(
+                              tag: 'movie_${movie.id}',
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black26,
+                                      blurRadius: 6,
+                                      offset: Offset(0, 2),
+                                    ),
+                                  ],
+                                ),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Image.network(
+                                    'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                                    width: 125,
+                                    height: 200,
+                                    fit: BoxFit.cover,
+                                    loadingBuilder: (
+                                      context,
+                                      child,
+                                      loadingProgress,
+                                    ) {
+                                      if (loadingProgress == null) return child;
+                                      return Center(
+                                        child: CircularProgressIndicator(
+                                          color: Colors.white70,
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 5),
+                            Text(
+                              movie.title.length > 14
+                                  ? '${movie.title.substring(0, 10)}...'
+                                  : movie.title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white70,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                openBuilder: (context, action) => DetailScreen(movie: movie),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
 }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,30 +1,31 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
-import 'package:pilem/models/movie.dart';
 
-class ApiServices{
+class ApiServices {
   static const String apiKey = '7347875a8052b695fd47aab9682244ab';
   static const String baseUrl = 'https://api.themoviedb.org/3';
 
-  Future<List<Movie>> getAllMovies() async{
-    final response = await http.get(Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"),
-    );
+  Future<List<Map<String, dynamic>>> getAllMovies() async {
+    final response = await http.get(Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"));
     final data = json.decode(response.body);
-    return (data['results'] as List).map((e) => Movie.fromJson(e)).toList();
+    return (data['results'] as List).map((e) => e as Map<String, dynamic>).toList();
   }
 
-  Future<List<Movie>> getTrendingMovies() async{
-    final response = await http.get(Uri.parse("$baseUrl/trending/movie/week?api_key=$apiKey"),
-    );
+  Future<List<Map<String, dynamic>>> getTrendingMovies() async {
+    final response = await http.get(Uri.parse("$baseUrl/trending/movie/week?api_key=$apiKey"));
     final data = json.decode(response.body);
-    return (data['results'] as List).map((e) => Movie.fromJson(e)).toList();
+    return (data['results'] as List).map((e) => e as Map<String, dynamic>).toList();
   }
 
-  Future<List<Movie>> getPopularMovies() async{
-    final response = await http.get(Uri.parse("$baseUrl/movie/popular?api_key=$apiKey"),
-    );
+  Future<List<Map<String, dynamic>>> getPopularMovies() async {
+    final response = await http.get(Uri.parse("$baseUrl/movie/popular?api_key=$apiKey"));
     final data = json.decode(response.body);
-    return (data['results'] as List).map((e) => Movie.fromJson(e)).toList();
+    return (data['results'] as List).map((e) => e as Map<String, dynamic>).toList();
+  }
+
+  Future<List<Map<String, dynamic>>> searchMovies(String query) async {
+    final response = await http.get(Uri.parse("$baseUrl/search/movie?query=$query&api_key=$apiKey"));
+    final data = json.decode(response.body);
+    return (data['results'] as List).map((e) => e as Map<String, dynamic>).toList();
   }
 }
